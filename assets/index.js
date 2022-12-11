20d90cb0086ffde8bf3735501fbc691ac99ce435
lhlyu <misakafs@gmail.com>
2022-12-11 09:46:35 +0800

    update

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,45 @@
-const splitTags = "。？！……——"
+
+
+const opens = ['(','[','{','<',"'",'"','（','【','《','『','「', '“', "‘"]
+const closes = [')',']','}','>',"'",'"','）','】','》','』','」', '”', "’"]
+
+const ends = ['.','!','?','。','！','？','\n']
+
+function splitText(text) {
+
+    const lines = []
+    let open = false
+    let start = 0
+    for (let i = 0; i < text.length; i++) {
+        const c = text.charAt(i)
+        if (opens.includes(c)) {
+            open = true
+        }
+        if (closes.includes(c)) {
+            open = false
+        }
+        if (open) {
+            continue
+        }
+        if (ends.includes(c)) {
+            const t = `<p>${text.slice(start, i+1).trim()}</p>`
+            lines.push(t)
+            start = i + 1
+        }
+    }
+    return lines
+}
+
+
+
 
 window.onload = () => {
 
     const one = document.querySelector('#one')
-    const two = document.querySelector('#two')
-
     const textarea = document.querySelector('textarea')
+
+    const two = document.querySelector('#two')
+    const container = document.querySelector('#container')
 
 
     // 清理文本
@@ -29,32 +63,9 @@ window.onload = () => {
             return
         }
 
-        const lines = text.split(/[\n]+/g)
+        const lines = splitText(text)
 
-        const ps = []
-
-        lines.forEach((line) => {
-            line = line.trim()
-            if (line === '') {
-                return
-            }
-            const length = line.match(/[。？！]+/g)?.length ?? 0
-            if (length <= 1) {
-                ps.push(`<p>${line}</p>`)
-                return
-            }
-            const rows = line.split(/[。？！]+/g)
-            rows.forEach((row) => {
-                row = row.trim()
-                if (row === '') {
-                    return
-                }
-                ps.push(`<p>${row}</p>`)
-            })
-        })
-
-        two.innerHTML = ps.join('\n')
-
+        container.innerHTML = lines.join('\n')
 
         one.classList.remove('show')
         one.classList.add('hide')
@@ -63,6 +74,27 @@ window.onload = () => {
     })
 
 
+    document.querySelector('#top').addEventListener('click', () => {
+        container.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    })
 
+
+    document.querySelector('#back').addEventListener('click', () => {
+        two.classList.remove('show')
+        two.classList.add('hide')
+        one.classList.remove('hide')
+        one.classList.add('show')
+    })
+
+
+    document.querySelector('#bottom').addEventListener('click', () => {
+        container.scrollTo({
+            top: container.scrollHeight,
+            behavior: 'smooth'
+        })
+    })
 
 }
